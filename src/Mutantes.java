b5d6798460e69729f0d9332cadf96bda1658a210
Ricardo Varjão <rvarjao@gmail.com>
2018-11-06 17:36:44 -0200

    Adicionada TelaInicial

--- src/Mutantes.java
+++ src/Mutantes.java
@@ -1,6 +1,9 @@
 class Mutantes {
     public static void main(String args[]){
-        Territorio territorio = new Territorio("Mutantes");
-        territorio.jogar();
+        TelaInicial telaInicial = new TelaInicial();
+
+//         Ricardo alterando a tela inicial
+        //        Territorio territorio = new Territorio("Mutantes");
+//        territorio.jogar();
     }
 }
--- src/TelaInicial.java
+++ src/TelaInicial.java
@@ -1,2 +1,73 @@
-public class TelaInicial {
+import sun.tools.jstat.Alignment;
+
+import javax.swing.*;
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+import javax.imageio.ImageIO;
+import java.io.File;
+import java.io.IOException;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.KeyListener;
+import java.nio.Buffer;
+import java.util.ArrayList;
+import javax.swing.BorderFactory;
+import javax.swing.border.Border;
+
+
+public class TelaInicial extends JPanel {
+
+    int largura = 400;
+    int altura = 400;
+
+    TelaInicial() {
+
+        JFrame frame = new JFrame();
+        frame.add(this);
+        frame.setSize(largura, altura);
+        frame.setResizable(false);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setBackground(Color.black);
+        this.setBackground(Color.black);
+
+        try {
+            BufferedImage image = ImageIO.read(new File("src/imagens/mutantes_logo.png"));
+            JLabel picLabel = new JLabel(new ImageIcon(image));
+
+            this.add(picLabel);
+
+            BufferedImage imgBtnJogar = ImageIO.read(new File("src/imagens/botao_jogar.png"));
+
+            Border emptyBorder = BorderFactory.createEmptyBorder();
+
+            JButton btnJogar = new JButton();
+            btnJogar.setBorder(emptyBorder);
+            btnJogar.setIcon(new ImageIcon(imgBtnJogar));
+            this.add(btnJogar);
+
+
+            BufferedImage imgBtnHalldaFama = ImageIO.read(new File("src/imagens/botao_hall_da_fama.png"));
+
+            JButton btnHallOfFame = new JButton();
+            btnHallOfFame.setBorder(emptyBorder);
+            btnHallOfFame.setIcon(new ImageIcon(imgBtnHalldaFama));
+            this.add(btnHallOfFame);
+
+        } catch (IOException ex) {
+            // handle exception...
+        }
+
+        frame.setVisible(true);
+
+
+
+
+
+
+    }
+
+
+
+
 }
