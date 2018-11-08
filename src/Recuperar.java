f77083c14e274dc7f1763c274728e1a32ffe122b
CHFernandes <chfernandes01@hotmail.com>
2018-11-08 13:18:45 -0200

    Add files via upload

---
+++ src/Recuperar.java
@@ -0,0 +1,16 @@
+//
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+
+public class Recuperar {
+//    public void recuperar(String nome_arquivo) throws IOException, ClassNotFoundException{
+//        /* objeto */ = null;
+//        FileInputStream arquivo = new FileInputStream(nome_arquivo);
+//        ObjectInputStream restaurador = new ObjectInputStream(arquivo);
+//        /* variavel */ = (/*objeto*/) restaurador.readObject();
+//        restaurador.close();
+//        arquivo.close();
+//    }
+}
